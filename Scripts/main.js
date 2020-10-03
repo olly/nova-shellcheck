247e17f7aeeeaae53f31d9f9c913106ad2f7b338
Olly Legg <olly@51degrees.net>
2020-10-03 14:16:11 +0100

    Lint document with `shellcheck`
    This uses the `IssueAssistant` interface, however this seems to be run
    twice, and cause duplicate issues to be reported.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,35 +1,75 @@
+exports.activate = function () {
+  // Do work when the extension is activated
+};
 
-exports.activate = function() {
-    // Do work when the extension is activated
-}
-
-exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
-}
-
+exports.deactivate = function () {
+  // Clean up state before the extension is deactivated
+};
 
 class IssuesProvider {
-    constructor() {
-        
+  provideIssues(editor) {
+    console.info("validating: ", editor.document.path);
+    if (!editor.document.path) {
+      return;
     }
-    
-    provideIssues(editor) {
-        let issues = [];
-        
-        // Create a new issue
+
+    let process = new Promise((resolve, _) => {
+      var process = new Process("shellcheck", {
+        shell: true,
+        args: ["--format", "json", editor.document.path],
+      });
+
+      let buffer = "";
+      process.onStdout(function (line) {
+        buffer = buffer + line;
+      });
+
+      process.onStderr(function (line) {
+        console.warn(line);
+      });
+
+      process.onDidExit(function () {
+        resolve(buffer);
+      });
+
+      process.start();
+    });
+
+    return process.then(function (stdout) {
+      return JSON.parse(stdout).map(function (data, i) {
+        console.info(i, data.message);
+
         let issue = new Issue();
-        
-        issue.message = "Invalid syntax: Missing semicolon";
-        issue.severity = IssueSeverity.Warning;
-        issue.line = 4;
+
+        issue.message = data.message;
+
+        switch (data.level) {
+          case "error":
+            issue.severity = IssueSeverity.Error;
+            break;
+          case "warning":
+            issue.severity = IssueSeverity.Warning;
+            break;
+          case "info":
+            issue.severity = IssueSeverity.Info;
+            break;
+          case "style":
+            issue.severity = IssueSeverity.Hint;
+            break;
+        }
+
+        issue.line = data.line;
+        issue.endLine = data.endLine;
+        issue.column = data.column;
+        issue.endColumn = data.endColumn;
         issue.column = 0;
-        
-        issues.push(issue);
-        
-        return issues;
-    }
+
+        return issue;
+      });
+    });
+  }
 }
 
-
-nova.assistants.registerIssueAssistant("html", new IssuesProvider());
-
+nova.assistants.registerIssueAssistant("shell", new IssuesProvider(), {
+  event: "onSave",
+});
